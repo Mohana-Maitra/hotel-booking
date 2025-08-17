b2d0a45588c22d9430b61c7aff2b65edbcf3464a
Mohana-Maitra <mohanamaitra24@gmail.com>
2025-08-17 09:02:17 +0530

    first commit

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -3,10 +3,10 @@ import { assets, cities } from '../assets/assets'
 
 const Hero = () => {
   return (
-    <div className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url("/src/assets/heroImage.png")] bg-no-repeat bg-cover bg-center h-screen'>
-      <p className='bg-[#49b9FF]/50 px-3.5 py-1 rounded-full mt-20'>The Ultimate Hotel Experience</p>
-      <h1 className='font-playfair text-2xl md:text-5xl md:text-[56px] md:leading-[56px] font-bold md:font-extrabold max-w-xl mt:4'>Discover Your Perfect Gateway Destination</h1>
-      <p className='max-w-130 mt-2 text-sm md:text-base'>Unparalleled luxury and comnfort await at the world's most exclusive hotels and resorts. Start your journey today.</p>
+    <div className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 bg-[url("/src/assets/heroImage.png")] bg-no-repeat bg-cover bg-center h-screen'>
+      <p className='bg-[#49b9FF]/50 px-3.5 py-1 text-white rounded-full mt-20'>The Ultimate Hotel Experience</p>
+      <h1 className='font-playfair text-2xl md:text-5xl md:text-[56px] md:leading-[56px]  text-white font-bold md:font-extrabold max-w-xl mt:4'>Discover Your Perfect Gateway Destination</h1>
+      <p className='max-w-130 mt-2 text-sm md:text-base text-white'>Unparalleled luxury and comnfort await at the world's most exclusive hotels and resorts. Start your journey today.</p>
 
       <form className='bg-white text-gray-500 rounded-lg px-6 py-4 mt-8 flex flex-col md:flex-row max-md:items-start gap-4 max-md:mx-auto'>
 
